7bf841a8deefc30f38dc31ec37cd560c628319ee
Nico Vijlbrief <nico.vijlbrief@outlook.com>
2023-06-27 16:10:01 +0200

    opdracht 7 works

--- OpenCV-opd7/main.cpp
+++ OpenCV-opd7/main.cpp
@@ -13,51 +13,99 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	Mat image, greyimage;  // images are stored in datatype Mat (matrix)
+    Mat image, greyImage, greyfilteredImage, edges; // images are stored in datatype Mat (matrix)
+    
+ if (argc < 2)
+    {
+        cout << "Usage: ./program_name <image_path>" << endl;
+        return -1;
+    }
 
-	// Read the image
-	image = imread(argv[1], IMREAD_COLOR);
-	if (!image.data)
-	{
-		cout << "Could not open or find " << endl;
-		return -1;
-	}
+    image = imread(argv[1], IMREAD_COLOR);
+    if (!image.data)
+    {
+        cout << "Could not open or find the image." << endl;
+        return -1;
+    }
 
-	// Convert the image from color to gray values: 
-	cvtColor(image, greyimage, COLOR_BGR2GRAY);
-
-       // Reduce noise:
-	GaussianBlur(greyimage, greyimage, Size(9, 9), 2, 2);
-
-	// Storage for blobs
-    vector<KeyPoint> pins;
-
-	// Setup SimpleBlobDetector parameters.
-    SimpleBlobDetector::Params params;
-
-	// Set up detector with params
-    Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create(params);
-
-    // Detect blobs
-    detector->detect(greyimage, pins);
-
-    Mat image_pins;
-    drawKeypoints(greyimage, pins, image_pins, Scalar(0, 0, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    // Convert the image from color to grayscale
+    cvtColor(image, greyImage, COLOR_BGR2GRAY);
+    //greyImage = image; //They are gray already
 
 
+    // Simple brightness and contrast controll. legally borrowed from 
+    // https://docs.opencv.org/3.4/d3/dc1/tutorial_basic_linear_transform.html
+    
+    Mat new_image = Mat::zeros( greyImage.size(), greyImage.type() );
+    /*double alpha = 1.1; /*< Simple contrast control */
+    /*int beta = 2; /*< Simple brightness control */
+    /*
+     for( int y = 0; y < greyImage.rows; y++ ) {
+         for( int x = 0; x < greyImage.cols; x++ ) {
+            for( int c = 0; c < greyImage.channels(); c++ ) {
+               new_image.at<Vec3b>(y,x)[c] = saturate_cast<uchar>( alpha * greyImage.at<Vec3b>(y,x)[c] + beta );
+            }
+        }
+    }*/
+    new_image = greyImage;
 
-	// Print (cout) the number of pins in the image (legaly borrowed from example)
-	if (pins.size() >= 1) {
-		cout << pins.size() << " pins detected!" << endl;
-	}
-	else {
-		cout << "No pins detected!" << endl;
-	}
+    // Reduce noise
+    GaussianBlur(new_image, greyfilteredImage, Size(9, 9), 2, 2);
 
-    // Show blobby circles
-	namedWindow("pins", WINDOW_AUTOSIZE);
-    imshow("pins", image_pins);
-	waitKey(0);
-	return 0;
+    // Le magic
+    threshold(greyfilteredImage, greyfilteredImage, 160, 255, THRESH_BINARY);
+    
+    // Detecteer edges met Canny
+    int lowThreshold = 50;      //50 ai, 20 ?
+    int highThreshold = 150;    //150 ai, 376 ?
+    Canny(greyfilteredImage, edges, lowThreshold, highThreshold);
+
+    // Apply morph
+    Mat kernel = getStructuringElement(MORPH_RECT, Size(5, 120)); //5, 120 worked "ok", originally MORPH_ELLIPSE
+    Mat morph;
+    morphologyEx(edges, morph, MORPH_CLOSE, kernel);
+
+    // vind de randen
+    vector<vector<Point>> contours;
+    vector<Vec4i> hierarchy;
+    findContours(morph, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+    // vind en tel de pinnen
+    int straightPinsCount = 0;
+    for (const auto& contour : contours)
+    { 
+        RotatedRect rect = minAreaRect(contour);
+        double areaRatio = abs(contourArea(contour)) / (rect.size.width * rect.size.height);
+        if (areaRatio > 0.60 && ((rect.size.height > 150 && rect.size.width < 12) || ( rect.size.width > 150 && rect.size.height < 12 ))) //(ratio >  0.95 and height > 5) 
+        {
+            straightPinsCount++;
+            drawContours(image, vector<vector<Point>>{contour}, -1, Scalar(0, 0, 255), 2);
+            
+            cout << "rect length " << rect.size.height << " with " << rect.size.width << endl;
+            Point2f points[4];
+            rect.points(points);
+            for (int i = 0; i < 4; i++)
+            {
+                line(image, points[i], points[(i + 1) % 4], Scalar(0, 255, 0), 2);
+                
+            }
+        }
+    }
+ 
+    // Hoeveel pinnen hadden we?
+
+    cout << "Number of straight pins: " << straightPinsCount << endl;
+   
+    namedWindow("filtered image", WINDOW_AUTOSIZE);
+    imshow("filtered image", greyfilteredImage);
+
+    waitKey(0);
+   
+    namedWindow("Detected Pins", WINDOW_AUTOSIZE);
+    imshow("Detected Pins", image);
+
+    waitKey(0);
+
+    return 0;
 }
 
