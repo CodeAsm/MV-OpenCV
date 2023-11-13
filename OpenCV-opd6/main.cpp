70561554959e6b91dd72691150736cee06fae0a0
Nico Vijlbrief <nico.vijlbrief@outlook.com>
2023-11-13 16:34:41 +0100

    Het menu en laden werkt

--- OpenCV-opd6/main.cpp
+++ OpenCV-opd6/main.cpp
@@ -1,64 +1,80 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
-
-
 using namespace cv;
 using namespace std;
 
+ // Convert naar grijswaarden
+    Mat gray, src_gray, image; // images are stored in datatype Mat (matrix)
+    int thresh = 100;
+    RNG rng(12345);
+
+
+void thresh_callback(int, void*);
 
 int main(int argc, char** argv)
 {
-    Mat train1, train2, image; // images are stored in datatype Mat (matrix)
+   
     
- if (argc > 1)
+ if (argc > 2)
     {
         cout << "Usage: ./program_name" << endl;
         return -1;
     }
 
-    train1 = imread("OCR/Image 01.bmp", IMREAD_GRAYSCALE);
-    if (!train1.data)
-    {
-        cout << "Could not open or find the first training file." << endl;
-        return -1;
-    }
-	train2 = imread("OCR/Image 02.bmp", IMREAD_GRAYSCALE);
-    if (!train2.data)
-    {
-        cout << "Could not open or find the second training file." << endl;
-        return -1;
-    }
-	image = imread("OCR/Image 03.bmp", IMREAD_GRAYSCALE);
+     image = imread(argv[1], IMREAD_COLOR);
     if (!image.data)
     {
         cout << "Could not open or find the image." << endl;
         return -1;
     }
+     // Laad een voorbeeldafbeelding
 
-    // Reduce noise
-   // GaussianBlur(new_image, greyfilteredImage, Size(9, 9), 2, 2);
 
-    // Le magic    
-    //threshold(train1, train1, 160, 255, THRESH_BINARY);
-    //threshold(train2, train2, 160, 255, THRESH_BINARY);
-    //threshold(image, image, 160, 255, THRESH_BINARY);
-    
-    
-   
-    namedWindow("Train 1", WINDOW_AUTOSIZE);
-    imshow("Train 1", train1);
+    cvtColor(image, gray, COLOR_BGR2GRAY);
 
+
+    // Binariseer de afbeelding
+    threshold(gray, gray, 128, 255, THRESH_BINARY);
+
+    blur( gray , src_gray, Size(3,3) );
+
+    // Identificeer en isoleer de contouren
+    std::vector<std::vector<Point>> contours;
+    findContours(src_gray, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+    // Loop over de contouren en teken rechthoeken rond de karakters
+    for (const auto& contour : contours) {
+        Rect boundingRect = cv::boundingRect(contour);
+        rectangle(image, boundingRect, Scalar(0, 255, 0), 2);
+    }
+    const char* source_window = "Source";
+    const int max_thresh = 255;
+    imshow( source_window, image );
+
+    createTrackbar( "Canny thresh:", source_window, &thresh, max_thresh, thresh_callback, NULL);
+    thresh_callback( 0, 0 );
+
+    // Toon de afbeelding met de geïdentificeerde karakters
+    imshow("Character Segmentation", image);
     waitKey(0);
-    namedWindow("Train 2", WINDOW_AUTOSIZE);
-    imshow("Train 2", train2);
+}
 
-    waitKey(0);
-    namedWindow("Detected char", WINDOW_AUTOSIZE);
-    imshow("Detected char", image);
+void thresh_callback(int, void* )
+{
 
-    waitKey(0);
-
-    return 0;
+    cout << "Current Threshold: " << thresh << endl;
+    Mat canny_output;
+    Canny( src_gray, canny_output, thresh, thresh*2 );
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    findContours( canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE );
+    Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+    for( size_t i = 0; i< contours.size(); i++ )
+    {
+        Scalar color = Scalar( rng.uniform(0, 256), rng.uniform(0,256), rng.uniform(0,256) );
+        drawContours( drawing, contours, (int)i, color, 2, LINE_8, hierarchy, 0 );
+    }
+    imshow( "Contours", drawing );
 }
 
